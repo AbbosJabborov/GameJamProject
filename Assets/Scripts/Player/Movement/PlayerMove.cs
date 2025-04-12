96ccbb76c43e88ac27661295874b90588e5fed41
Claive <abbosjabborov12@gmail.com>
2025-04-12 10:52:54 +0500

    Update: .gitignore and empty scripts

---
+++ Assets/Scripts/Player/Movement/PlayerMove.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Player.Movement
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
